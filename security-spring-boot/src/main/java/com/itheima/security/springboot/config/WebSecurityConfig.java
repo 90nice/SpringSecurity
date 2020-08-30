748cba0f2da38c9337ef8377ec334938b60336a2
Wenxue <wenxue.xiang@ingka.com>
2020-08-31 01:31:10 +0800

    钩子测试6

--- security-spring-boot/src/main/java/com/itheima/security/springboot/config/WebSecurityConfig.java
+++ security-spring-boot/src/main/java/com/itheima/security/springboot/config/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //    public PasswordEncoder passwordEncoder(){
 //        return NoOpPasswordEncoder.getInstance();
 //    }
-
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
